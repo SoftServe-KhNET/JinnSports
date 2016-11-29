a74175f55e650eeba0b7e50201c18423da98bba5
Denis Polozhyi <my_email@whatever.com>
2016-11-29 19:21:41 +0300

    Added service with parsers threads(not init yet) and Service Installer

---
+++ JinnSports.ParserService/ParserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JinnSports.ParserService
+{
+    public partial class ParserService : ServiceBase
+    {
+        Thread jsonParserThread;
+
+        Thread htmlParserThread;
+
+        public ParserService()
+        {
+            InitializeComponent();
+            jsonParserThread = new Thread(() => { });
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            jsonParserThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            jsonParserThread.Abort();
+        }
+    }
+}
