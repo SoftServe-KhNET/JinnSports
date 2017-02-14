a5bdd8570f26fcfe8140e3f9861cdea56cd51c3b
ivan-rataichuk <ivan.rataichuk@gmail.com>
2017-02-14 21:32:42 +0200

    added SPA component

---
+++ JinnSports.WEB/Scripts/Jinn/init.js
@@ -0,0 +1,39 @@
+﻿(function() {
+    'use strict';
+
+    var jinn = null;
+
+    // On page loaded
+    document.addEventListener('DOMContentLoaded', function() {
+        jinn = new JinnApp();
+
+        var authModel = new authModel();
+        var authView = new AuthView(authModel);
+
+        jinn._addElements(authView, authModel);
+
+        jinn.router = new jinn.Router({
+
+            map: {
+                '': 'index'
+            },
+
+            index: function () {
+                this.hideAllViews();
+                authView.show();
+            },
+
+            hideAllViews: function () {
+                var views = jinn._views;
+                for (var viewId in views) {
+                    if (views.hasOwnProperty(viewId)) {
+                        views[viewId].hide();
+                    }
+                }
+            }
+
+        });
+
+        jinn._run('#');
+    });
+})();
